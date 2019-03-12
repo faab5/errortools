7e002aec386f7c97b8f030633fa510168db33c7e
Eva van Weel <evavanweel@Evas-MacBook-Pro-2.local>
2019-03-12 16:57:29 +0100

    Create logistic regression class structure

---
+++ errortools/logistic_regression.py
@@ -0,0 +1,22 @@
+class LogisticRegression():
+    def __init__(self):
+        pass
+    
+    def fit(self, X, y):
+        pass
+    
+    def predict(self, X):
+        pass
+    
+    def predict(self, X, w):
+        pass
+    
+    def estimate_errors(self, X):
+        pass
+    
+    def _check_inputs(self, X, y, w):
+        pass
+    
+    
+    
+    
