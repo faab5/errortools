bfe30e5655dc56273462dab1918e5e569a348cf5
Faab <fabian.jansen@cblab.int>
2019-03-13 10:44:56 +0100

    Add method _check_inputs and unit tests

--- errortools/logistic_regression.py
+++ errortools/logistic_regression.py
@@ -1,8 +1,16 @@
+import numpy as np
+import iminuit
+import scipy.stats
+
 class LogisticRegression():
+    """
+
+    """
+
     def __init__(self):
         pass
-    
-    def fit(self, X, y):
+
+    def fit(self, X, y, w0=0, fit_intercept=True):
         pass
     
     def predict(self, X):
@@ -13,9 +21,70 @@ class LogisticRegression():
     
     def estimate_errors(self, X):
         pass
-    
-    def _check_inputs(self, X, y, w):
-        pass
+
+    def _check_inputs(self, X, w=None, y=None, fit_intercept=True):
+        """
+        Check inputs for matching dimensions and convert to numpy arrays
+
+        Add a column of ones to X if fit_intercept
+        Assure y consists of zeros and ones
+
+        :param X: feature matrix
+        :param w: weight vector
+        :param y: target vector
+        :param fit_intercept:
+        :return: X, w, y as numpy arrays
+        """
+        if y is not None:
+            X = np.atleast_2d(X)
+            y = (np.atleast_1d(y) != 0).astype(int)
+            w = np.zeros(1, dtype=float) if w is None else np.atleast_1d(w)
+
+            if X.ndim > 2:
+                raise ValueError("Dimension of features X bigger than 2 not supported")
+            if y.ndim > 1:
+                raise ValueError("Dimension of target y bigger than 1 not supported")
+            if w.ndim > 1:
+                raise ValueError("Dimension of weights w bigger than 1 not supported")
+
+            if X.shape[0] == 1 and X.shape[1] == y.shape[0]:
+                # X could have been 1D and stored as a row
+                # in stead of a column. If so transpose X.
+                X = X.T
+            elif X.shape[0] != y.shape[0]:
+                raise ValueError("Number of data points in features X and target y don't match")
+
+            if fit_intercept == True:
+                X = np.concatenate((X, np.ones((X.shape[0], 1), dtype=float)), axis=1)
+
+            if w.shape[0] == 1:
+                # w could have been given as a number not an array
+                # if so expand it to the width of X
+                w = np.full(X.shape[1], w[0], dtype=float)
+
+            if w.shape[0] != X.shape[1]:
+                raise ValueError("Dimension of weights w does not match number of features X")
+
+        else:
+            X = np.atleast_2d(X)
+            if fit_intercept == True:
+                X = np.concatenate((X, np.ones((X.shape[0], 1), dtype=float)), axis=1)
+
+            w = np.zeros(1, dtype=float) if w is None else np.atleast_1d(w)
+            if w.shape[0] == 1:
+                # w could have been given as a number not an array
+                # if so expand it to the width of X
+                w = np.full(X.shape[1], w[0], dtype=float)
+
+            if X.ndim > 2:
+                raise ValueError("Dimension of features X bigger than 2 not supported")
+            if w.ndim > 1:
+                raise ValueError("Dimension of weights w bigger than 1 not supported")
+
+            if w.shape[0] != X.shape[1]:
+                raise ValueError("Dimension of weights w does not match number of features X")
+
+        return X, w, y
     
     
     
--- tests/test_stuff.py
+++ tests/test_stuff.py
@@ -1,11 +1,101 @@
-from sklearn.linear_model import LogisticRegression
-#from errortools.lofo_importance import LOFOImportance, plot_importance
-from data.test_data import generate_test_data, generate_unstructured_test_data
+import errortools
+import numpy as np
 
+def test_logistic_regression_check_inputs():
+    estimator = errortools.LogisticRegression()
 
-def test_stuff():
-    df = generate_test_data(1000)
+    # check shapes of X, w and y
+    X = np.random.uniform(size=4*3).reshape(4,3)
+    w = np.random.uniform(size=3)
+    y = np.random.choice([0,1], size=4)
+    A, u, b = estimator._check_inputs(X, w, y, fit_intercept=False)
+    assert u.shape[0] == A.shape[1]
+    assert b.shape[0] == A.shape[0]
 
-    features = ["A", "B", "C", "D"]
+    # check shapes of X, w and y
+    X = np.random.uniform(size=4*3).reshape(4,3)
+    w = np.random.uniform(size=4)
+    y = np.random.choice([0,1], size=4)
+    estimator._check_inputs(X, w, y, fit_intercept=True)
+    assert u.shape[0] == A.shape[1]
+    assert b.shape[0] == A.shape[0]
 
-    assert True==True, "Message if assert fails"
+    # check shapes of X, w and y
+    # when weights are given as a single number
+    # _check_inputs will expand the weights to
+    # the right dimension
+    X = np.random.uniform(size=4 * 3).reshape(4, 3)
+    w = 0
+    y = np.random.choice([0,1], size=4)
+    A, u, b = estimator._check_inputs(X, w, y, False)
+    assert u.shape[0] == A.shape[1]
+    assert b.shape[0] == A.shape[0]
+
+    A, u, b = estimator._check_inputs(X, w, y, True)
+    assert u.shape[0] == A.shape[1]
+    assert b.shape[0] == A.shape[0]
+
+    # check shapes of X, w and y
+    # when X is given as a 1D vector
+    # in stead of 2D matrix
+    # _check_inputs will reshape X
+    # to fit the target
+    X = np.random.uniform(size=3)
+    w = np.random.uniform(size=4)
+    y = 1
+    A, u, b = estimator._check_inputs(X, w, y, True)
+    assert u.shape[0] == A.shape[1]
+    assert b.shape[0] == A.shape[0]
+
+    X = np.random.uniform(size=3)
+    w = np.random.uniform(size=2)
+    y = np.random.choice([0,1], size=3)
+    A, u, b = estimator._check_inputs(X, w, y, True)
+    assert u.shape[0] == A.shape[1]
+    assert b.shape[0] == A.shape[0]
+
+    # check shapes of X, w and y
+    # when X is given as a 1D vector
+    # and w as a single number
+    # _check_inputs will expand X to match y
+    # and expand w to match X
+    X = np.random.uniform(size=3)
+    w = 0
+    y = 1
+    A, u, b = estimator._check_inputs(X, w, y, True)
+    assert u.shape[0] == A.shape[1]
+    assert b.shape[0] == A.shape[0]
+
+    X = np.random.uniform(size=3)
+    w = 0
+    y = np.random.choice([0,1], size=3)
+    A, u, b = estimator._check_inputs(X, w, y, True)
+    assert u.shape[0] == A.shape[1]
+    assert b.shape[0] == A.shape[0]
+
+    # check shapes of X and w
+    # when y is not given
+    X = np.random.uniform(size=2*3).reshape(2,3)
+    w = np.random.uniform(size=4)
+    y = None
+    A, u, _ = estimator._check_inputs(X, w, y, True)
+    assert u.shape[0] == A.shape[1]
+
+    # check shapes of X and w
+    # when y is not given
+    # and X is given as a 1D array
+    X = np.random.uniform(size=3)
+    w = np.random.uniform(size=4)
+    y = None
+    A, u, _ = estimator._check_inputs(X, w, y, True)
+    assert u.shape[0] == A.shape[1]
+
+    # check shapes of X and w
+    # when y is not given
+    # and X is given as a 1D array
+    # and w is given as a single number
+    X = np.random.uniform(size=3)
+    w = 0
+    y = None
+    A, u, _ = estimator._check_inputs(X, w, y, True)
+    assert u.shape[0] == A.shape[1]
