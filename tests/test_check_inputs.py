c4a7c89a74fa82471d4dcca2ba87e2040df320f5
Faab <fabian.jansen@cblab.int>
2019-03-19 11:11:45 +0100

    Refactor of fit, predict and estimate errors

--- tests/test_check_inputs.py
+++ tests/test_check_inputs.py
@@ -6,27 +6,37 @@ np.random.seed(42)
 def test_logistic_regression_check_inputs():
     estimator = errortools.LogisticRegression()
 
-    # check shapes of X, y, weights w and bias b
+    # check shapes of X, y, weights w
     X = np.random.uniform(size=4 * 3).reshape(4,3)
     w = np.random.uniform(size=3)
     y = np.random.choice([0,1], size=4)
-    b = 0
-    Xp, yp, wp, bp = estimator._check_inputs(X, y, w, b)
+    Xp, yp, wp = estimator._check_inputs(X, y, w, False)
     assert wp.shape[0] == Xp.shape[1]
     assert yp.shape[0] == Xp.shape[0]
 
-    # check shapes of X, y, weights w and bias b
+    # check shapes of X, y, weights w
     # when weights are given as a single number
     # _check_inputs will expand the weights to
     # the right dimension
     X = np.random.uniform(size=4 * 3).reshape(4, 3)
     w = 0
     y = np.random.choice([0,1], size=4)
-    Xp, yp, wp, bp = estimator._check_inputs(X, y, w, b)
+    Xp, yp, wp = estimator._check_inputs(X, y, w, False)
     assert wp.shape[0] == Xp.shape[1]
     assert yp.shape[0] == Xp.shape[0]
 
-    # check shapes of X, y, weights w and bias b
+    # check shapes of X, y, weights w
+    # when weights are given as a single number
+    # _check_inputs will expand the weights to
+    # the right dimension
+    X = np.random.uniform(size=4 * 3).reshape(4, 3)
+    w = 0
+    y = np.random.choice([0,1], size=4)
+    Xp, yp, wp = estimator._check_inputs(X, y, w, True)
+    assert wp.shape[0] == Xp.shape[1]
+    assert yp.shape[0] == Xp.shape[0]
+
+    # check shapes of X, y, weights w
     # when X is given as a 1D vector
     # in stead of 2D matrix
     # _check_inputs will reshape X
@@ -34,7 +44,7 @@ def test_logistic_regression_check_inputs():
     X = np.random.uniform(size=3)
     w = np.random.uniform(size=3)
     y = 1
-    Xp, yp, wp, bp = estimator._check_inputs(X, y, w, b)
+    Xp, yp, wp = estimator._check_inputs(X, y, w, False)
     assert wp.shape[0] == Xp.shape[1]
     assert yp.shape[0] == Xp.shape[0]
 
@@ -46,14 +56,14 @@ def test_logistic_regression_check_inputs():
     X = np.random.uniform(size=3)
     w = 0
     y = 1
-    Xp, yp, wp, bp = estimator._check_inputs(X, y, w, b)
+    Xp, yp, wp = estimator._check_inputs(X, y, w, True)
     assert wp.shape[0] == Xp.shape[1]
     assert yp.shape[0] == Xp.shape[0]
 
     X = np.random.uniform(size=3)
     w = 0
     y = np.random.choice([0,1], size=3)
-    Xp, yp, wp, bp = estimator._check_inputs(X, y, w, b)
+    Xp, yp, wp = estimator._check_inputs(X, y, w, True)
     assert wp.shape[0] == Xp.shape[1]
     assert yp.shape[0] == Xp.shape[0]
 
@@ -62,16 +72,16 @@ def test_logistic_regression_check_inputs():
     X = np.random.uniform(size=2*3).reshape(2,3)
     w = np.random.uniform(size=3)
     y = None
-    Xp, _, wp, bp = estimator._check_inputs(X, y, w, b)
+    Xp, _, wp = estimator._check_inputs(X, y, w, False)
     assert wp.shape[0] == Xp.shape[1]
 
     # check shapes of X and w
     # when y is not given
     # and X is given as a 1D array
     X = np.random.uniform(size=3)
-    w = np.random.uniform(size=3)
+    w = np.random.uniform(size=4)
     y = None
-    Xp, _, wp, bp = estimator._check_inputs(X, y, w, b)
+    Xp, _, wp= estimator._check_inputs(X, y, w, True)
     assert wp.shape[0] == Xp.shape[1]
 
     # check shapes of X and w
@@ -81,5 +91,5 @@ def test_logistic_regression_check_inputs():
     X = np.random.uniform(size=3)
     w = 0
     y = None
-    Xp, _, wp, bp = estimator._check_inputs(X, y, w, b)
+    Xp, _, wp = estimator._check_inputs(X, y, w, True)
     assert wp.shape[0] == Xp.shape[1]
--- tests/test_fit.py
+++ tests/test_fit.py
@@ -4,16 +4,16 @@ import scipy.stats
 
 np.random.seed(42)
 
-w_true = np.array([1, 0, -0.25])
-w_true_normed = w_true/np.sqrt(np.sum(w_true**2))
-ndata = 1000
+def test_fit_predict_and_estimate_errors():
+    w_true = np.array([1, 0, -0.25])
+    b_true = 1.
+    ndata = 1000
 
-X = np.random.uniform(low=-1, high=1, size=2*ndata).reshape((ndata, 2))
-X_with_intercept = np.concatenate((X, np.ones((ndata,1), dtype=float)), axis=1)
-y = (scipy.stats.logistic.cdf(np.dot(X_with_intercept, w_true)) > np.random.uniform(size=ndata)).astype(int)
+    X = np.random.uniform(low=-1, high=1, size=len(w_true) * ndata).reshape((ndata, len(w_true)))
+    y = (scipy.stats.logistic.cdf(np.dot(X, w_true) + b_true) > np.random.uniform(size=ndata)).astype(int)
 
-
-model = errortools.LogisticRegression(fit_intercept=True, l1=0, l2=0)
-model.fit(X, y, w0=0)
-model.predict(X)
-model.estimate_errors(X)
+    model = errortools.LogisticRegression(fit_intercept=True, l1=0, l2=0)
+    model.fit(X, y, w0=0)
+    model.predict(X)
+    model.estimate_errors(X)
+    assert True
